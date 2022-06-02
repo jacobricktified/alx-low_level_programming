9af2a000fea0aafc302c4e33adcc3ba22fcb59a0
jacob mbuco <mbucojacob@gmail.com>
2022-06-02 10:23:59 -0700

    Print text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - the name of the function that is called when the program starts.
+*
+*Return: 0  showing that the program ran successfully.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
