78a7ae8bab1d36af287ea2e5cd4314ede4662085
jacob mbuco <mbucojacob@gmail.com>
2022-06-02 06:43:52 -0700

    Print text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /*
- Write a C program that prints exactly \"Programming is like building a multilingual puzzle, followed by a new line. 
-Use the function puts
-You are not allowed to use printf
-Your program should end with the value 0
-time complexity is O(1) because time taken to print is constant 
-space complexity is O(1) because no extra space is used             */
+ Write a C program that prints exactly \" Programming is like building a multilingual puzzle, followed by a new line.
+ Use the function puts
+ You are not allowed to use printf
+ Your program should end with the value 0
+ time complexity is O(1) because time taken to print is constant 
+ space complexity is O(1) because no extra space is used
+output: \"Programming is like building a multilingual puzzle, followed by a new line. 
+ */
 
-int main() // its the first function that gets called
+int main()
 {
-	    puts("Programming is not  like building a multilingual puzzle, followed by a new line."); //this line prints the string
-	    return 0;; // this line returns 0
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
 }
