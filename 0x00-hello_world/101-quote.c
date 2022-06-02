9136ef6a7400ab3356d877e21fd64c327f3e01ea
jacob mbuco <mbucojacob@gmail.com>
2022-06-02 14:41:18 -0700

    Print text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main -the name of the function that is called when the program starts.
+ *
+ *Return: 0 showing that the program ran successfully.
+ *
+ *time complexity: O(1)
+ *
+ *space complexity: O(1)
+ *
+ */
+
+int main(void)
+{
+	 write(1, "and that piece of art is useful - ", 34);
+	 write(1, "Dora Korpar, 2015-10-19\n", 24);
+	return (0);
+}
