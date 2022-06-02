54c2ee4d2b40cefe65e94c869b6e5a3718a69e10
jacob mbuco <mbucojacob@gmail.com>
2022-06-02 14:50:46 -0700

    Write funtion in one line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,7 +14,6 @@
 
 int main(void)
 {
-	 write(1, "and that piece of art is useful - ", 34);
-	 write(1, "Dora Korpar, 2015-10-19\n", 24);
+	 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
