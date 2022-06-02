e70bb1074ba0c3716eb5dfb79bb1de3c6b93f28d
jacob mbuco <mbucojacob@gmail.com>
2022-06-02 12:29:28 -0700

    Rearrange order of exectution

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
+	char            this_char;
+	int             this_int;
+	long            this_long;
+	long long       this_longlong;
 	float		this_float;
-	int		this_int;
-	long		this_long;
-	long long	this_longlong;
-	char		this_char;
 
-	printf("Size of a float: %lu byte(s)\n", sizeof(this_float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(this_char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(this_int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(this_long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(this_longlong));
-	printf("Size of a char: %lu byte(s)\n", sizeof(this_char));
+	printf("Size of a float: %lu byte(s)\n", sizeof(this_float));
 
 	return (0);
 }
