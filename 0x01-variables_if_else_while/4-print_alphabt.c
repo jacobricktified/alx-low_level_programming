d3b9808969941538b607e017698b5de178edc0f7
jacob mbuco <mbucojacob@gmail.com>
2022-06-05 12:36:09 -0700

    a program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-/*
- * betty style doc for function main goes there
- *
- * main - is the start point of the program that will Print all the letters except q and e
- *
- * Return: 0 means the program is successful
- *
- * space time complexity is O(1)
- *
- * time complexity is O(1)
- */
+/**
+*main - is the start point of the program
+*
+*Return: 0 means the program is successful
+*
+*Description: Print all the letters except q and e
+*
+*space time complexity is O(1)
+*
+*time complexity is O(1)
+*/
 
 int main(void)
 {
-		char alpha;
+	char alpha;
 
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
-		{
-		if (alpha != 'e' &&  != 'q')
-		putchar(alpha);
-        putchar('\n');
-		}
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	if (alpha != 'e' &&  != 'q')
+	putchar(alpha);
+	putchar('\n');
+	}
 
 	return (0);
 }
