f91ac4542eac9abc350cef16d90524ef51bb9847
jacob mbuco <mbucojacob@gmail.com>
2022-06-05 08:52:23 -0700

    Remove extra files

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /*
  * betty style doc for function main goes there
  *
- * main - is the start point of the program that will that prints the alphabet in lowercase
+ * main - is the start point of the program that will that prints the alphabet in lowercase, followed by a new line
  * 
  * Return: 0 means the program is successful
  *
