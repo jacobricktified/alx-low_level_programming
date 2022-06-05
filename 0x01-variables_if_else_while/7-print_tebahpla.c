6f55554d54c53553e724e949f7cb9a8f4ebb4538
jacob mbuco <mbucojacob@gmail.com>
2022-06-05 13:56:53 -0700

    Print lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * betty style doc for function main goes there
- *
- * main - is the start point of the program that prints the lowercase alphabet in reverse
- *
- * Return: 0 means the program is successful
- *
- * space time complexity is O(1)
- *
- * time complexity is O(1)
- */
+*main - is the start point of the program
+*
+* Description:  prints the lowercase alphabet in reverse
+*
+*Return: 0 means the program is successful
+*
+*space time complexity is O(1)
+*
+*time complexity is O(1)
+*/
 int main(void)
 {
 	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
-    {
-	    putchar(letter);
-	    putchar('\n');
-    }
+	{
+	putchar(letter);
+	putchar('\n');
+	}
 
 	return (0);
 }
