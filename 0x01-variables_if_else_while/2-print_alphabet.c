b75d8a12a12bf906bfb94d67e6016d1661a35af2
jacob mbuco <mbucojacob@gmail.com>
2022-06-05 09:51:26 -0700

    Print alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
-/*
- * betty style doc for function main goes there
- *
- * main - is the start point of the program that will that prints the alphabet in lowercase, followed by a new line
- * 
- * Return: 0 means the program is successful
- *
- * space time complexity is O(1)
- *
- * time complexity is O(1)
- */
+
+/**
+*main - is the start point of the program
+*
+*Description : Print the alphabet in lowercase
+*
+*Return: 0 means the program is successful
+*
+*space time complexity is O(1)
+*
+*time complexity is O(1)
+*/
 int main(void)
 {
 
 	char alf;
 
 	for (alf = 'a'; alf <= 'z'; alf++)
-    {
+	{
 		putchar(alf);
 		putchar('\n');
-    }
-    return (0);
+	}
+	return (0);
 }
