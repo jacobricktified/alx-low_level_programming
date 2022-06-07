67bdfc5963f5a43d528f438e38cd03e84693e86b
jacob mbuco <mbucojacob@gmail.com>
2022-06-08 00:01:54 +0300

    Add code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
 /**
-*main - Start of the program 
-*
-*Description:  a program that prints _putchar
+*main - start of the program
 *
 *Return: 0 means the program is successful
 *
-*time complexity is O(1)
+*Descrition: print the last digit of the number stored in the variable n
 *
-*space complexity is O(1)
+*space time complexity is O(1)
+*
+*time complexity is O(1)
 */
 
 int main(void)
 {
-    _putchar();
+	char this[8] ="_putchar";
+	int i;
+
+	for(i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar('\n');
+	return 0;
 }
