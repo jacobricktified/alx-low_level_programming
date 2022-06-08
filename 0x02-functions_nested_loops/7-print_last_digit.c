123dac0fc9fc6ca2a80d632cd3343c59869b7def
jacob mbuco <mbucojacob@gmail.com>
2022-06-08 04:00:54 -0700

    Rewrite code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
- * @c: integer parameter
- * Return: value of the last digit
+ *  * print_last_digit - Function
+ *   *
+ *    * Description: prints the last digit of a number
+ *     *
+ *      * @n: parameter of type int, name n.
+ *       *
+ *        * Return: returns value of last digit
  */
-int print_last_digit(int c)
+
+int print_last_digit(int n)
 {
-	int b;
+	int l;
 
-	if (c < 0)
-	{
-		c = c * -1;
-		b = c % 10;
-	}
+	if (n < 0)
+		l = -1 * (n % 10);
 	else
-	{
-		b = c % 10;
-	}
+	l = n % 10;
 
-	_putchar(b + '0');
-
-	return (b);
+	_putchar((l % 10) + '0');
+	return (l % 10);
 }
