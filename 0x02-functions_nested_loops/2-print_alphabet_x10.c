26c9979db49fef0c9006bb5f3d9de77ba3e34a42
jacob mbuco <mbucojacob@gmail.com>
2022-06-07 17:31:01 -0700

    Add 1-main.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*main - start of the program
+*
+*Return: 0 means the program is successful
+*
+*Descrition: a program that prints _putchar
+*
+*space time complexity is O(1)
+*
+*time complexity is O(1)
+*/
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+	while (ch <= 'z')
+	{
+	_putchar(alpha);
+		ch++;
+	}
+	_putchar('\n');
+		i++;
+	}
+}
