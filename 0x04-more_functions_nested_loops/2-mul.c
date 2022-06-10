f773067dac51689a712308e354a4d756435e8e01
jacob mbuco <mbucojacob@gmail.com>
2022-06-09 23:16:37 -0700

    Multiply two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,6 @@
 
 /**
  * mul - multiplies two integers
- * @c: store the result of the multiplication
  * @i: store the first number
  * @j: store the second number
  * Return: the result of the multiplication
