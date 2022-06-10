7d68eceaeecb967bd4803b42312e84ccf53d68e3
jacob mbuco <mbucojacob@gmail.com>
2022-06-10 01:29:56 -0700

    Include the right header file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - checks for uppercase character
  * @c: the character to be checked
- * Return: 1 if c is uppercase else 0 otherwise
- *
+ * Return: 1 if c is uppercase else  0 otherwise
  */
 int _isupper(int c)
 {
