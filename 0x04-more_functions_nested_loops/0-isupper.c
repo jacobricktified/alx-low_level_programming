ddd3608be6f95ffe1e700790c23da7003a63e3b6
jacob mbuco <mbucojacob@gmail.com>
2022-06-09 23:08:07 -0700

    check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,28 +6,28 @@
  *
  * @c: parameter of the function is an int type
  *
- * Return: returns 1 if c is uppercase else 0 
+ * Return: returns 1 if c is uppercase else 0
  */
 
 int _isupper(int c)
 {
-    int result;
-    int upper = 'A';
-    int lower = 'a';
+	int result;
+	int upper = 'A';
+	int lower = 'a';
 
-    for (upper = 'A'; upper <= 'Z'; upper++)
-    {
-        if (c == upper)
-        {
-            result = 1;
-        }
-        for (lower = 'a'; lower <= 'z'; lower++)
-            {
-            if (c == lower)
-            {
-                result = 0;
-            }
-            }
-    }
-    return (result);
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+	if (c == upper)
+	{
+		result = 1;
+	}
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (c == lower)
+		{
+			result = 0;
+		}
+	}
+	}
+	return (result);
 }
