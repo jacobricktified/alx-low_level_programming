7134f92a74aeb4fe26ae2ce7ca87b0b9e5fb202f
jacob mbuco <mbucojacob@gmail.com>
2022-06-10 08:51:19 +0300

    Write code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @c: store the result of the multiplication
+ * @i: store the first number
+ * @j: store the second number
+ * Return: the result of the multiplication
+ */
+int mul(int i, int j)
+{
+	int c;
+
+	c = i * j;
+
+	return (c);
+}
