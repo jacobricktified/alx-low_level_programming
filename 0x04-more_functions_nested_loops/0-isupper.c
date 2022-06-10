67f5fc5869b903a92ac771e5e88532f178193876
jacob mbuco <mbucojacob@gmail.com>
2022-06-10 01:15:05 -0700

    Try another method

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,11 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* _isupper -  checks for uppercase character
-* @c: int to be checked
-* Return: 1 if c is uppercase else 0 otherwise
-*/
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase else 0 otherwise
+ */
 int _isupper(int c)
 {
-char upper = 'A';
-int res = 0;
-for (; upper <= 'Z'; uppe++)
-{
-if (c == upper)
-{
-res = 1;
-break;
-}
-}
-return (res);
+	return (c >= 'A' && c <= 'Z');
 }
