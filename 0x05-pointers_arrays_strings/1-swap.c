022ac8a99dec661073ce61d93498bac58ebf8bd0
jacob mbuco <mbucojacob@gmail.com>
2022-06-13 17:16:50 -0700

    Swap value of two int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,14 @@
 #include "main.h"
-extern int make_iso_compilers_happy;
- /**
- * @brief: swap_int - swaps the values of two integers
- * @a: pointer to an int
- * @b: pointer to an int
- * Description: swaps the values of two integers
- * output: a=98 , b=402 , a=402 , b=98
- * @return: void
- */
+
+/**
+* swap_int - swaps the values of two integers
+* @a: pointer to an int
+* @b: pointer to an int
+* Description: swaps the values of two integers
+* output: a=98 , b=402 , a=402 , b=98
+* Return: void(nothing)
+*/
+
 void swap_int(int *a, int *b)
 {
 	int n = *a;
