a6dae8243a924bf4fc5c2f787ffbcf56dd7ee596
jacob mbuco <mbucojacob@gmail.com>
2022-06-13 17:15:06 -0700

    Set int pointer to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
-extern int make_iso_compilers_happy;
+
 /**
-* @brief: reset_to_98-takes a pointer to an int and swaps it with the value at the memory address
+* reset_to_98 - takes a pointer to an int and
+* swaps it with the value at the memory address
 * @n: pointer to an int
 * Description: takes a pointer to an int and sets it to 98
 * output: n=402 , n=98
 */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
