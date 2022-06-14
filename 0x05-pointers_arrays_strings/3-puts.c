67b9dde8460162123096029b6fff0447aabac99d
jacob mbuco <mbucojacob@gmail.com>
2022-06-13 17:20:42 -0700

    Print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <unistd.h>
- /**
- * @brief: _puts - This function prints the string to stdout
- * @str: pointer to the string
- * @output: I do not fear computers. I fear the lack of them - Isaac Asimov
- * Description: This function prints the string to stdout
- * Return: void (nothing)
- *
- */
+
+/**
+* _puts - This function prints the string to stdout
+* @str: pointer to the string
+* output: I do not fear computers. I fear the lack of them - Isaac Asimov
+* Description: This function prints the string to stdout
+* Return: void (nothing)
+*
+*/
+
 void _puts(char *str)
 {
 	int len = 0;
