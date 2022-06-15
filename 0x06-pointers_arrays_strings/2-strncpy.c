75f6e53262bac2502da03574ca3d588ddbe41093
jacob mbuco <mbucojacob@gmail.com>
2022-06-15 15:34:39 -0700

    Copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @src: pointer parameter of type char that will be copied from
  * @n: parameter of type int that represent to the number of bytes to copy
  * Return: pointer to dest string
- * Description: a function that copies a string
+ * Description: a function that copies a string.
 */
 char *_strncpy(char *dest, char *src, int n)
 {
