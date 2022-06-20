998ef396627b542df1a2413767e603a02d6b3b10
jacob mbuco <mbucojacob@gmail.com>
2022-06-20 15:14:03 -0700

    Print the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,7 +4,7 @@
  * print_chessboard - pointer to the array of chars 
  * @a: pointer to the array of chars 
  * REturn: void (no return)
- * Description: function that prints a chessboard .
+ * Description: function that prints a chessboard.
  */
 void print_chessboard(char (*a)[8])
 {
