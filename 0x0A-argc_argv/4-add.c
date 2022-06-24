4fbcefa424db4a695e6cf0756f0289cfce99a27f
jacob mbuco <mbucojacob@gmail.com>
2022-06-24 10:31:09 -0700

    Add positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,10 +3,10 @@
 #include <ctype.h>
 
 /**
- * main - entry
+ * main - Start of program
  * @argc: length of command line input
  * @argv: array of command line argument
- * Return: 0 if success
+ * Return: 0 if success.
  */
 int main(int argc, char const *argv[])
 {
@@ -31,3 +31,4 @@ int main(int argc, char const *argv[])
 
 	return (0);
 }
+
